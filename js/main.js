8ca5841505696d3dc5c72958e3a0ba039852639f
Legend-of-iPhoenix <32944537+Legend-of-iPhoenix@users.noreply.github.com>
2017-11-12 16:15:34 -0500

    reworked auth

--- js/main.js
+++ js/main.js
@@ -1,44 +1,28 @@
-var provider = new firebase.auth.GoogleAuthProvider();
-
-var userID;
-function assignUID()
+var username = "anonymous";
+function assignUsername()
 {
-  //var fireNewID = firebase.database().ref().child("NewID");
-  firebase.auth().signInWithRedirect(provider);
-  
-  firebase.auth().getRedirectResult().then(function(result) {
-  if (result.credential) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // ...
-  }
-  // The signed-in user info.
-  var user = result.user;
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ..
-  console.log("error-code:"+errorCode);
-  console.log("error-message:"+errorMessage);
-  console.log("email:"+email);
-  console.log("credential:"+credential);
-});
+  var adj = ["Anonymous","Small", "Red","Orange","Yellow","Blue","Indigo","Violet","Shiny","Sparkly","Large","Hot","Cold","Evil","Kind","Ugly","Legendary"];
+  var noun = ["Bear", "Dog","Cat","Banana","Pepper","Bird","Lion","Apple","Phoenix","Diamond","Jewel","Person","Whale","Plant","Duckling","Thing"];
+
+  var rAdj = Math.floor(Math.random()*adj.length);
+  var rNoun = Math.floor(Math.random()*noun.length);
+var name = adj[rAdj] + noun[rNoun];
+  var u = prompt("Please Enter Your Username:", name);
+  u = u.replace(/[\'\"\;]/g,'');
+  document.cookie = "unichat_uid="+u+";expires=Tue, 19 Jan 2038 03:14:07 UTC";
+  alert("Username set to \'"+u+"\'");
+  username = u;
 }
 
-assignUID();
+assignUsername();
 
 function submitMessage() {
 	var database = firebase.database();
   var messageBox = document.getElementById("message");
   database.ref("Data").push({
     value: messageBox.value,
-    timestamp: Date.now()//,
-    //UID: userID
+    timestamp: Date.now(),
+    userName: username;
   });
   messageBox.value = "";
 }
