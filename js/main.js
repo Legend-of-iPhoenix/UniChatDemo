fdc118be877e51016bcad2d60cc3a9c5d27dd25f
Legend-of-iPhoenix <32944537+Legend-of-iPhoenix@users.noreply.github.com>
2017-11-13 16:37:41 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,51 @@
 var username = "anonymous";
 function assignUsername()
 {
-  var adj = ["Anonymous","Small", "Red","Orange","Yellow","Blue","Indigo","Violet","Shiny","Sparkly","Large","Hot","Cold","Evil","Kind","Ugly","Legendary","Flaming"];
-  var noun = ["Bear", "Dog","Cat","Banana","Pepper","Bird","Lion","Apple","Phoenix","Diamond","Jewel","Person","Whale","Plant","Duckling","Thing","Flame"];
+  var adj = ["Anonymous","Small", "Red","Orange","Yellow","Blue","Indigo","Violet","Shiny","Sparkly","Large","Hot","Cold","Evil","Kind","Ugly","Legendary","Flaming","Salty","Slippery"];
+  var noun = ["Bear", "Dog","Cat","Banana","Pepper","Bird","Lion","Apple","Phoenix","Diamond","Jewel","Person","Whale","Plant","Duckling","Thing","Flame","Number","Cow","Dragon","Hedgehog"];
 
   var rAdj = Math.floor(Math.random()*adj.length);
   var rNoun = Math.floor(Math.random()*noun.length);
-var name = adj[rAdj] + noun[rNoun];
-  var u = prompt("Please Enter Your Username:", name);
-  u = u.replace(/[\'\"\;]/g,'');
-	if (u == "_iPhoenix_")
-	{
-		u = name;
-	}
-  document.cookie = "unichat_uid="+u+";expires=Tue, 19 Jan 2038 03:14:07 UTC";
-  alert("Username set to \'"+u+"\'");
-  username = u;
+  var name = adj[rAdj] + noun[rNoun];
+  return name;
 }
 
-assignUsername();
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    var u = getCookie("unichat_uid");
+    if (u != "") {
+        alert("Welcome back to UniChat, " + u);
+    } else {
+        u = prompt("Please Enter Your Username:", assignUsername());
+        if (u != "" && u != null) {
+            setCookie("unichat_uid", u, 2*365);
+        }
+    }
+    return u;
+}
+
+username = checkCookie();
 
 
 function submitMessage() {
