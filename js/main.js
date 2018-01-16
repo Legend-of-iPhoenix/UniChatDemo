94d072ccd25b2019818deb8625b4c181fc4eaf49
LAX18 <34464552+LAX18@users.noreply.github.com>
2018-01-16 13:56:08 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -619,4 +619,5 @@ function deleteAllCookies() {
         var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
         document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
     }
+    location.reload();
 }
