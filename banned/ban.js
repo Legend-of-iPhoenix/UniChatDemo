2af8ae93ab7b2dcea748e8925b306c5f1fb35aec
Legend-of-iPhoenix <32944537+Legend-of-iPhoenix@users.noreply.github.com>
2018-01-17 20:11:39 -0500

    Cleaned up teh codez.

--- banned/ban.js
+++ banned/ban.js
@@ -5,7 +5,7 @@ function getMessage(tag) {
   return string ? string[1] : null;
 };
 
-window.onload = function() {
+window.onload = function () {
   var message = getMessage("m");
   var until = getMessage("t");
   console.warn(message);
@@ -14,31 +14,32 @@ window.onload = function() {
   } else {
     document.getElementById("adminMessage").innerHTML = "Message: " + message.replace(/%([0-9]{2})/g, " &#$1 ");
   }
-  setInterval(function() {
-    document.getElementById("timeLeft").innerHTML = formatTime(until-Date.now());
+  setInterval(function () {
+    document.getElementById("timeLeft").innerHTML = formatTime(until - Date.now());
   }, 1000);
 }
 
 function formatTime(ts) {
   if (ts > 0) {
 
-  	var sec = ts / 1000,
-  	min = sec / 60,
-  	hour = min / 60,
-  	day = hour / 24;
-  	sec %= 60;
-  	min %= 60;
-  	hour %= 24;
-  	function format(n,unit) {
-  		n = Math.floor(n);
-  		return n +" "+ unit + (n==1 ? "" : "s") + ", "
-  	}
-  	sec = format(sec,"second");
-  	min = format(min,"minute");
-  	hour = format(hour,"hour");
-  	day = format(day,"day");
-  	sec = sec.substring(0, sec.length - 2);
-  	var result = day + hour + min + "and "+sec + "."
+    var sec = ts / 1000,
+      min = sec / 60,
+      hour = min / 60,
+      day = hour / 24;
+    sec %= 60;
+    min %= 60;
+    hour %= 24;
+
+    function format(n, unit) {
+      n = Math.floor(n);
+      return n + " " + unit + (n == 1 ? "" : "s") + ", "
+    }
+    sec = format(sec, "second");
+    min = format(min, "minute");
+    hour = format(hour, "hour");
+    day = format(day, "day");
+    sec = sec.substring(0, sec.length - 2);
+    var result = day + hour + min + "and " + sec + "."
     return result;
   } else {
     return 'Your ban has expired. Click <a href="../index.html">here</a> to go back to UniChat.';
