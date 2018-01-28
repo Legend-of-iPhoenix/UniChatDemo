a24489bc98ba3f4c91aae6e56c52cfe11fa69e3d
jcgter777 :P <34496783+jcgter777@users.noreply.github.com>
2018-01-28 18:04:22 -0500

    Added 123outerme as a bot contributor :P

--- profile/js/main.js
+++ profile/js/main.js
@@ -1,7 +1,7 @@
 //Thanks, guys. It means a lot. - The Development Team
 var closedBetaTesters = ["SM84CE", "TheLastMillennial", "TLM", "Battlesquid", "xMarminq_"];
 
-var bot_contributors = ["SM84CE", "jcgter777", "womp"];
+var bot_contributors = ["SM84CE", "jcgter777", "womp", "123outerme"];
 
 var helpers = ["LAX18","MrDKKing","jcgter777","Drewster30","mets11rap"];
 
